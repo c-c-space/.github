279e246e6d47983b4979b5f459d52d7387672bc0
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-22 05:34:10 +0900

    Update script.js

--- sp/org/script.js
+++ sp/org/script.js
@@ -1,5 +1,4 @@
-// arr1
-let arr1 = {
+let weight = {
   'must' : '100 ~ 90',
   'should' : '90 ~ 70',
   'can' : '70 ~ 50',
@@ -8,23 +7,7 @@ let arr1 = {
   'cant' : '10 ~ 0',
 }
 
-const arrBtn1 = document.querySelector('#weight')
-Object.entries(arr1).forEach(eachArr1 => {
-  const arrInput1 = document.createElement ('input')
-  arrInput1.setAttribute('type', 'radio')
-  arrInput1.setAttribute('name', 'weight')
-  arrInput1.setAttribute('id', eachArr1[0])
-  arrInput1.value = eachArr1[0]
-  arrBtn1.appendChild(arrInput1)
-
-  const arrLabel1 = document.createElement ('label')
-  arrLabel1.setAttribute('for', eachArr1[0])
-  arrLabel1.innerText = eachArr1[1]
-  arrBtn1.appendChild(arrLabel1)
-})
-
-// arr2
-let arr2 = {
+let size = {
   'positive' : '+',
   'negative' : '-',
   'both' : '±',
@@ -32,23 +15,7 @@ let arr2 = {
   'unknown' : '?',
 }
 
-const arrBtn2 = document.querySelector('#size')
-Object.entries(arr2).forEach(eachArr2 => {
-  const arrInput2 = document.createElement ('input')
-  arrInput2.setAttribute('type', 'radio')
-  arrInput2.setAttribute('name', 'size')
-  arrInput2.setAttribute('id', eachArr2[0])
-  arrInput2.value = eachArr2[0]
-  arrBtn2.appendChild(arrInput2)
-
-  const arrLabel2 = document.createElement ('label')
-  arrLabel2.setAttribute('for', eachArr2[0])
-  arrLabel2.innerText = eachArr2[1]
-  arrBtn2.appendChild(arrLabel2)
-})
-
-// arr3
-let arr3 = {
+let feel = {
   'happy' : '🙂',
   'hearts' : '🥰',
   'tongue' : '😜',
@@ -61,24 +28,37 @@ let arr3 = {
   'steam' : '😤',
 }
 
-const arrBtn3 = document.querySelector('#feel')
-Object.entries(arr3).forEach(eachArr3 => {
-  const arrInput3 = document.createElement ('input')
-  arrInput3.setAttribute('type', 'radio')
-  arrInput3.setAttribute('name', 'feel')
-  arrInput3.setAttribute('id', eachArr3[0])
-  arrInput3.value = eachArr3[0]
-  arrBtn3.appendChild(arrInput3)
+radio(weight, "weight");
+radio(size, "size");
+radio(feel, "feel");
 
-  const arrLabel3 = document.createElement ('label')
-  arrLabel3.setAttribute('for', eachArr3[0])
-  arrLabel3.innerText = eachArr3[1]
-  arrBtn3.appendChild(arrLabel3)
-})
+org("feel");
 
-// .list li の表示・非表示
-function multi_filter(org = "") {
-  let radioAll = document.querySelectorAll(org)
+// ラジオボタンの生成
+function radio(arr, name = "") {
+  let orgEach = arr + "Each"
+  let orgRadio = arr + "Radio"
+  let orgLabel = arr + "Label"
+
+  let orgInput = document.querySelector(`#${name}`)
+  Object.entries(arr).forEach(orgEach => {
+    const orgRadio = document.createElement ('input')
+    orgRadio.setAttribute('type', 'radio')
+    orgRadio.setAttribute('name', `${name}`)
+    orgRadio.setAttribute('id', orgEach[0])
+    orgRadio.value = orgEach[0]
+    orgInput.appendChild(orgRadio)
+
+    const orgLabel = document.createElement ('label')
+    orgLabel.setAttribute('for', orgEach[0])
+    orgLabel.innerText = orgEach[1]
+    orgInput.appendChild(orgLabel)
+  })
+}
+
+// アイテムの表示・非表示
+function org(org = "") {
+  let radioAll = document.querySelectorAll(`#${org} input[type='radio']`)
 
   for (let i of radioAll) {
     i.addEventListener('change', () => {
@@ -99,7 +79,3 @@ function multi_filter(org = "") {
     })
   }
 }
-
-multi_filter("#weight input[type='radio']");
-multi_filter("#size input[type='radio']");
-multi_filter("#feel input[type='radio']");
