98c9ff7c4a8645b24cbc1531167a72888848bd7e
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-22 01:42:26 +0900

    Update script.js

--- sp/org/script.js
+++ sp/org/script.js
@@ -76,7 +76,7 @@ Object.entries(arr3).forEach(eachArr3 => {
 });
 
 
-let radio_btns = document.querySelectorAll("input[type='radio']")
+let radio_btns = document.querySelectorAll("input[type='radio'][name='feel']")
 for (let target of radio_btns) {
   target.addEventListener('change', () => {
     let thisAll = document.querySelectorAll(`li.${target.value}`)
