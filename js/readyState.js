480a89886ead5d863fc1c7ebc4834b0c041f6dc0
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-30 16:19:57 +0900

    Update readyState.js

--- js/readyState.js
+++ js/readyState.js
@@ -55,7 +55,7 @@ document.addEventListener('readystatechange', event => {
 
       const backBtn = document.querySelector('#back-btn')
       backBtn.addEventListener('click', function () {
-        media.stop();
+        navigator.mediaDevices.stop();
 
         userMedia.remove()
       })
