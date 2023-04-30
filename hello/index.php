755a04d790fa9cecdf803fe0a58b38dc73a921bb
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-30 17:50:17 +0900

    Update index.php

--- hello/index.php
+++ hello/index.php
@@ -13,6 +13,10 @@
   <link rel="stylesheet" href="css/log.css" />
   <link rel="stylesheet" href="css/mobile.css" media="screen and (max-width: 750px)" />
   <style type="text/css">
+  body {
+    background: #fff;
+  }
+
   #menu {
     background: #000;
   }
