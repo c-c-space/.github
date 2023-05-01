fe2c7f1e5db0b2d3cff9c6af41886636c593b133
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-01 17:50:12 +0900

    Update index.php

--- thankyou/index.php
+++ thankyou/index.php
@@ -87,7 +87,7 @@
 
   <main id="hello">
     <h1>
-      <sup>Thank you for visiting</sup><br/>
+      <sup>Thank You for Visiting</sup><br/>
       <i class="hsl">Your Drawing is Seems So Beautiful</i>
     </h1>
     <section>
