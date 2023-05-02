d1a886cf785ef67fdd9600ca815a4db9489c523a
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-03 02:20:20 +0900

    Update index.js

--- profile/index.js
+++ profile/index.js
@@ -1,5 +1,19 @@
 const info = document.querySelector('#info')
 
+if(localStorage.getItem('geolocation')) {
+  const yourGEO = JSON.parse(localStorage.getItem('geolocation'))
+  const geolocation = document.createElement("li")
+  geolocation.innerHTML += `
+  <span>行ったことのない場所へ行く</span>
+  <span>
+  <button class="color bgcolor" onclick="location.assign('/map/')">Go Out</button>
+  </span>
+  <span></span>
+  <span>Latitude: <b>${yourGEO.latitude}°</b> Longitude: <b>${yourGEO.longitude}°</b></span>
+  `
+  info.before(geolocation)
+}
+
 if(localStorage.getItem('sign')) {
   const yourSign = JSON.parse(localStorage.getItem('sign'))
   const sign = document.createElement("li")
