1dca142c7e82315cc11a125504013313016b947f
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-03 02:09:58 +0900

    Update index.php

--- profile/index.php
+++ profile/index.php
@@ -65,8 +65,8 @@
       $hqdn = $_SERVER["REMOTE_PORT"];
       $os = $_SERVER["HTTP_USER_AGENT"];
 
-      echo "<span>IP <code id='ip'>" . $ip . "</code></span>";
       echo "<span>PORT <code id='hqdn'>" . $hqdn . "</code></span>";
+      echo "<span>IP <code id='ip'>" . $ip . "</code></span>";
       echo "<span>USER AGENT <code id='os'>" . $os . "</code></span>";
       ?>
     </li>
