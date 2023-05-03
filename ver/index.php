e50dfa5616dfefd28c2d46cf16845caab79ec8f7
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-03 23:48:01 +0900

    Update index.php

--- ver/index.php
+++ ver/index.php
@@ -17,10 +17,6 @@ function h($str) {
   <script src="/js/index.js" async></script>
   <link rel="stylesheet" href="rolling.css" />
   <style type="text/css">
-  #rolling :first-child {
-    width: 40%;
-  }
-
   #rolling :nth-child(2),
   #rolling :nth-child(3) {
     width: 30%;
