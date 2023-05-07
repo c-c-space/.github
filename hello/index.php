8bb4d86f234ddca3977744a1329132979fbf7e49
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-07 23:29:42 +0900

    hello

--- hello/index.php
+++ hello/index.php
@@ -4,12 +4,16 @@ mb_internal_encoding("UTF-8");
 date_default_timezone_set('Asia/Tokyo');
 
 if (date("H") >= 6 and date("H") <= 11) {
+  $greeting = "Good Morning おはよう";
   $timeframe = "morning";
 } elseif (date("H") >= 12 and date("H") <= 17) {
+  $greeting = "Hello こんにちは";
   $timeframe = "afternoon";
 } elseif (date("H") >= 18 and date("H") <= 23) {
+  $greeting = "Good Evening こんばんは";
   $timeframe = "evening";
 } else {
+  $greeting = "Good Night おやすみ";
   $timeframe = "night";
 }
 
--- hello/js/readyState.js
+++ hello/js/readyState.js
@@ -14,15 +14,6 @@ function ChangeHidden() {
 document.addEventListener('readystatechange', event => {
   if (event.target.readyState === 'loading') {
   } else if (event.target.readyState === 'complete') {
-    const greeting = document.querySelector('#log div h1 b')
-
-    let timeframe = (new Date()).getHours()
-    if (timeframe <= 5) {greeting.innerText = "Good Night おやすみ"}
-    else if (timeframe <= 11) {greeting.innerText = "Good Morning おはよう"}
-    else if (timeframe <= 17) {greeting.innerText = "Hello こんにちは"}
-    else if (timeframe <= 23 ) {greeting.innerText = "Good Evening こんばんは"}
-    else {greeting.innerText = "Good Night おやすみ"}
-
     const mainLog = document.querySelector('#log')
     const logAll = document.querySelector('#log section')
     const lastModified = document.querySelector('#lastModified')
