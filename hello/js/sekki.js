70b79ae5f1b5d3b3708f97f53b64795d8bc9c531
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-03 23:31:37 +0900

    Update sekki.js

--- hello/js/sekki.js
+++ hello/js/sekki.js
@@ -3,6 +3,13 @@
 const dateAll = document.querySelectorAll('#log ul li button')
 for (const dateLi of dateAll) {
   dateLi.addEventListener('click', function () {
+    const uttr = new SpeechSynthesisUtterance()
+    uttr.text = this.dataset.hello
+    uttr.lang = "ja-JP"
+    uttr.pitch = 1
+    uttr.rate = 1
+    speechSynthesis.speak(uttr)
+
     const sekkiName = document.querySelector('#log h1 b')
     sekkiName.innerText = this.innerText
 
@@ -14,23 +21,19 @@ for (const dateLi of dateAll) {
   }, false)
 }
 
-const helloLiAll = document.querySelectorAll('#log ul li button')
-for (const helloLi of helloLiAll) {
-  helloLi.addEventListener('click', function () {
-    const uttr = new SpeechSynthesisUtterance()
-    uttr.text = this.dataset.hello
-    uttr.lang = "ja-JP"
-    uttr.voice = speechSynthesis
-    .getVoices()
+// 発話の停止・一時停止・再開
+const cancelBtn = document.querySelector('#cancel-btn')
+const pauseBtn = document.querySelector('#pause-btn')
+const resumeBtn = document.querySelector('#resume-btn')
 
-    uttr.pitch = 1
-    uttr.rate = 1
-    speechSynthesis.speak(uttr)
+cancelBtn.addEventListener('click', function () {
+  speechSynthesis.cancel()
+})
 
-    const output = document.querySelector('#log h1 b')
-    output.innerText = this.dataset.hello
+pauseBtn.addEventListener('click', function () {
+  speechSynthesis.pause()
+})
 
-    const lastModified = document.querySelector('#lastModified')
-    lastModified.innerText = this.innerText
-  }, false)
-}
+resumeBtn.addEventListener('click', function () {
+  speechSynthesis.resume()
+})
