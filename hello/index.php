1f80032ae015d0c1888a708c562423be684b9207
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-03 17:50:31 +0900

    Update index.php

--- hello/index.php
+++ hello/index.php
@@ -1,3 +1,36 @@
+<?php
+mb_language("ja");
+mb_internal_encoding("UTF-8");
+date_default_timezone_set('Asia/Tokyo');
+
+if (date("H") >= 6 and date("H") <= 11) {
+  $greeting = "Good Morning おはよう";
+  $timeframe = "morning";
+} elseif (date("H") >= 12 and date("H") <= 17) {
+  $greeting = "Hello こんにちは";
+  $timeframe = "afternoon";
+} elseif (date("H") >= 18 and date("H") <= 23) {
+  $greeting = "Good Evening こんばんは";
+  $timeframe = "evening";
+} else {
+  $greeting = "Good Night おやすみ";
+  $timeframe = "night";
+}
+
+$source_file = "2023/". $timeframe . ".csv";
+
+function h($str) {
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+$fp = fopen($source_file, 'a+b');
+while ($row = fgetcsv($fp)) {
+  $rows[] = $row;
+}
+
+$post = count($rows);
+flock($fp, LOCK_SH);
+?>
 
 <!DOCTYPE html>
 <html lang="ja">
