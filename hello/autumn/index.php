3c28facb52d5fcb33cb9e058dfdf35818e0a48b3
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-03 23:23:22 +0900

    map

--- hello/autumn/index.php
+++ hello/autumn/index.php
@@ -36,6 +36,17 @@ $url = "{$site}" . "{$_SERVER['REQUEST_URI']}";
   <link rel="icon" href="/ver/icon.png" type="image/png">
   <link rel="icon" href="/ver/icon/android.png" sizes="192x192" type="image/png">
   <link rel="apple-touch-icon-precomposed" href="/ver/icon/apple.png" sizes="180x180" type="image/png">
+  <style media="screen">
+  #log h2 {
+    width: 95%;
+    max-width: 550px;
+  }
+
+  #log h2 {
+    font-size: 100%;
+    line-height: 150%;
+  }
+  </style>
 </head>
 
 <body>
@@ -54,9 +65,9 @@ $url = "{$site}" . "{$_SERVER['REQUEST_URI']}";
     <div>
       <h1>
         <code id="lastModified"><?php echo $date;?></code>
-        <b><?php echo $season;?></b>
+        <b><?php echo $description;?></b>
       </h1>
-      <h2><?php echo $description;?></h2>
+      <h2><?php echo $season;?></h2>
     </div>
     <section>
       <ul>
@@ -65,90 +76,62 @@ $url = "{$site}" . "{$_SERVER['REQUEST_URI']}";
             <date>Aug 8</date> -
             <date>Aug 22</date>
           </p>
-          <button type="button" data-date="8月8日 - 8月22日頃" data-hello="The Beginning of Autumn">
+          <button type="button" data-date="The Beginning of Autumn" data-hello="この日から立冬の前日までが秋。一年で一番暑い頃であるが、一番暑いと言うことはあとは涼しくなるばかり。暑中見舞いはこの前日まで、この日以降は残暑見舞い。">
             立秋 (<b>risshu</b>)
           </button>
-          <p>
-            この日から立冬の前日までが秋。<br/>
-            一年で一番暑い頃であるが、一番暑いと言うことはあとは涼しくなるばかり。<br/>
-            暑中見舞いはこの前日まで、この日以降は残暑見舞い。
-          </p>
-          <br/>
         </li>
         <li>
           <p>
             <date>Aug 23</date> -
             <date>Sep 7</date>
           </p>
-          <button type="button" data-date="8月23日 - 9月7日頃" data-hello="Hot weather abates">
+          <button type="button" data-date="Hot weather abates" data-hello="処暑は暑さが止むと言う意味。萩の花が咲き、朝夕は心地よい涼風が吹く頃だが、台風のシーズンでもある。">
             処暑 (<b>shosho</b>)
           </button>
-          <p>
-            処暑は暑さが止むと言う意味。<br/>
-            萩の花が咲き、朝夕は心地よい涼風が吹く頃だが、台風のシーズンでもある。
-          </p>
-          <br/>
         </li>
         <li>
           <p>
             <date>Sep 8</date> -
             <date>Sep 22</date>
           </p>
-          <button type="button" data-date="9月8日 - 9月22日頃" data-hello="Dew forms on the leaves">
+          <button type="button" data-date="Dew forms on the leaves" data-hello="野には薄の穂が顔を出し、秋の趣がひとしお感じられる頃。朝夕の心地よい涼風に、幾分の肌寒さを感じさせる冷風が混じり始める。">
             白露 (<b>hakuro</b>)
           </button>
-          <p>
-            野には薄の穂が顔を出し、秋の趣がひとしお感じられる頃。<br/>
-            朝夕の心地よい涼風に、幾分の肌寒さを感じさせる冷風が混じり始める。
-          </p>
-          <br/>
         </li>
         <li>
           <p>
             <date>Sep 23</date> -
             <date>Oct 7</date>
           </p>
-          <button type="button" data-date="9月23日 - 10月7日頃" data-hello="The Autumn Equinox">
+          <button type="button" data-date="The Autumn Equinox" data-hello="暑い日は減り代わりに冷気を感ずる日が増える。昼と夜の長さがほぼ同じになる、秋の七草が咲き揃う頃。">
             秋分 (<b>shuubun</b>)
           </button>
-          <p>
-            暑い日は減り代わりに冷気を感ずる日が増える。<br/>
-            昼と夜の長さがほぼ同じになる、秋の七草が咲き揃う頃。
-          </p>
-          <br/>
         </li>
         <li>
           <p>
             <date>Oct 8</date> -
             <date>Oct 23</date>
           </p>
-          <button type="button" data-date="10月8日 - 10月23日頃" data-hello="Weather becomes colder">
+          <button type="button" data-date="Weather becomes colder" data-hello="冷たい露の結ぶ頃。秋もいよいよ本番。菊の花が咲き始め、山の木々の葉は紅葉の準備に入る。">
             寒露 (<b>kanro</b>)
           </button>
-          <p>
-            冷たい露の結ぶ頃。秋もいよいよ本番。<br/>
-            菊の花が咲き始め、山の木々の葉は紅葉の準備に入る。
-          </p>
-          <br/>
         </li>
         <li>
           <p>
             <date>Oct 24</date> -
             <date>Nov 7</date>
           </p>
-          <button type="button" data-date="10月24日 - 11月7日頃" data-hello="The season of frost">
+          <button type="button" data-date="The Season of Frost" data-hello="北国や山間部では、霜が降りて朝には草木が白く化粧をする頃。野の花の数は減り始める、代わって山を紅葉が飾る頃である。">
             霜降 (<b>soukou</b>)
           </button>
-          <p>
-            北国や山間部では、霜が降りて朝には草木が白く化粧をする頃。<br/>
-            野の花の数は減り始める、代わって山を紅葉が飾る頃である。
-          </p>
-          <br/>
         </li>
       </ul>
     </section>
   </main>
   <script src="../js/sekki.js"></script>
+
+  <?php require('../php/controls.php'); ?>
+  <script src="../js/controls.js"></script>
 
   <dialog id="modal" class="color bgcolor">
     <h3><?php echo $season;?></h3>
--- hello/js/sekki.js
+++ hello/js/sekki.js
@@ -1,13 +1,15 @@
-const helloLiAll = document.querySelectorAll('#log ul li button')
-for (const helloLi of helloLiAll) {
-  helloLi.addEventListener('click', function () {
-    const sekki = document.querySelector('#log h1 b')
-    sekki.innerText = this.innerText
+'use strict'
 
-    const dates = document.querySelector('#lastModified')
-    dates.innerText = this.dataset.date
+const dateAll = document.querySelectorAll('#log ul li button')
+for (const dateLi of dateAll) {
+  dateLi.addEventListener('click', function () {
+    const sekkiName = document.querySelector('#log h1 b')
+    sekkiName.innerText = this.innerText
 
-    const about = document.querySelector('#log h2')
-    about.innerText = this.dataset.hello
+    const sekkiDates = document.querySelector('#lastModified')
+    sekkiDates.innerText = this.dataset.date
+
+    const sekkiAbout = document.querySelector('#log h2')
+    sekkiAbout.innerText = this.dataset.hello
   }, false)
 }
