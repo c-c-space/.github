e6419a400d0e615594330dfebd6e364dd1694108
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-04 14:50:32 +0900

    Update log.php

--- hello/all/log.php
+++ hello/all/log.php
@@ -1,5 +1,17 @@
 <section>
   <ul>
+    <li>
+      <p>
+        <code>Google 日本語 (ja-JP)</code>
+        <code>Pitch 0.9</code>
+        <code>Rate 0.9</code>
+      </p>
+      <p>
+        <button type="button" data-name="Google 日本語" lang="ja-JP" data-pitch="0.9" data-rate="0.9" data-hello="<?php echo $description;?>">
+          <?php echo $date;?>
+        </button>
+      </p>
+    </li>
     <?php if (!empty($rows)) : ?>
       <?php shuffle($rows); foreach ($rows as $row):?>
         <li>
