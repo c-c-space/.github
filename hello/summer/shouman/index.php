fe2d8aee6dd81e90637fa27ab5bca60931f8852c
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-04 22:48:22 +0900

    Update index.php

--- hello/summer/shouman/index.php
+++ hello/summer/shouman/index.php
@@ -1,6 +1,8 @@
 <?php
-
-$sekkiName = "小満 shouman";
+$season = "summer";
+$sekki = "shouman";
+$seasonName = "夏";
+$sekkiName = "小満";
 $date = "May 20 - Jun 4";
 $description = "Plants come into full leaf";
 $hello = "あらゆる生命が満ちていく頃。陽気がよくなり、草木などの生物が次第に生長して生い茂る。西日本でははしり梅雨が現れる。";
@@ -18,7 +20,7 @@ require_once('../../all/head.php');
       <h1>
         <b>
           <code><?php echo $greeting;?></code>
-          <?php echo $sekkiName;?>
+          <?php echo $sekkiName;?> <?php echo $sekki;?>
         </b><br/>
         <code id="lastModified"><?php echo $date;?></code>
       </h1>
@@ -36,15 +38,27 @@ require_once('../../all/head.php');
 
   <dialog id="modal" class="color bgcolor">
     <h3>
-      <small><?php echo $date;?></small><br/>
-      <?php echo $sekkiName;?>
+      <small><?php echo $description;?></small><br/>
+      <?php echo $sekkiName;?> <?php echo $sekki;?>
     </h3>
-    <p><?php echo $description;?></p>
     <button class="color bgcolor" id="closeButton">×</button>
+    <p><?php echo $date;?></p>
+    <p><?php echo $hello;?></p>
     <br/>
     <?php require('../../all/timeframe.php'); ?>
   </dialog>
-  <script src="../../../profile/js/jscolor.js"></script>
+  <script src="../color.js"></script>
+  <script type="text/javascript">
+  let namesForm = document.querySelectorAll('#bgcolor, #color')
+  for (const names of namesForm) {
+    Object.entries(colors).forEach(eachArr => {
+      let option = document.createElement('option')
+      option.textContent = Object.values(eachArr)[0]
+      option.value = Object.values(eachArr)[1]
+      names.appendChild(option)
+    })
+  }
+  </script>
   <script src="../../../profile/js/setStyles.js"></script>
 </body>
 </html>
