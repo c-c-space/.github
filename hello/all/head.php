41cf2b1b8ed91a04912b328256cfe410fdeca3e4
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-04 13:18:10 +0900

    Update head.php

--- hello/all/head.php
+++ hello/all/head.php
@@ -29,9 +29,23 @@ while ($row = fgetcsv($source_file)) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no" />
+  <title><?php echo $title; ?></title>
+  <meta name="description" content="<?php echo $description; ?>">
+  <meta property="og:title" content="<?php echo $title; ?>" />
+  <meta property="og:site_name" content="<?php echo $_SERVER['HTTP_HOST']; ?>" />
+  <meta property="og:url" content="<?php echo $url; ?>" />
+  <meta property="og:type" content="website" />
+  <meta property="og:locale" content="ja_JP" />
+  <meta property="og:image" content="<?php echo $url; ?>summary.png" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:image" content="<?php echo $url; ?>summary.png" />
+
   <link rel="stylesheet" href="../../style.css" />
   <link rel="stylesheet" href="../../css/index.css" />
   <link rel="stylesheet" href="../../css/controls.css" />
   <link rel="stylesheet" href="../../css/log.css" />
   <link rel="stylesheet" href="../../css/mobile.css" media="screen and (max-width: 750px)" />
+  <link rel="icon" href="/ver/icon.png" type="image/png">
+  <link rel="icon" href="/ver/icon/android.png" sizes="192x192" type="image/png">
+  <link rel="apple-touch-icon-precomposed" href="/ver/icon/apple.png" sizes="180x180" type="image/png">
 </head>
