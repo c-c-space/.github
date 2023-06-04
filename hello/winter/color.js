876f5dacbd49f71a572dbc11502504c2f9c34563
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-04 16:38:22 +0900

    Update color.js

--- hello/winter/color.js
+++ hello/winter/color.js
@@ -76,8 +76,8 @@ let namesForm = document.querySelectorAll('#bgcolor, #color')
 for (const names of namesForm) {
   for (let i = 0; i < colors.length; i++) {
     let option = document.createElement('option')
-    option.textContent = colors[i].keys()
-    option.value = colors[i].values()
+    option.textContent = Object.keys(colors[i])[0]
+    option.value = Object.values(colors[i])[0]
     names.appendChild(option)
   }
 }
