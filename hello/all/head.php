649aa353dfa5dbf3a508fe4a336dfe8838865e18
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-04 13:45:18 +0900

    Update head.php

--- hello/all/head.php
+++ hello/all/head.php
@@ -21,7 +21,7 @@ if (isset($_GET["timeframe"])) {
   $timeframe = $_GET["timeframe"];
 }
 
-$source_file = fopen($timeframe.".csv", 'r');
+$source_file = fopen($timeframe.".csv", 'a+');
 while ($row = fgetcsv($source_file)) {
   $rows[] = $row;
 }
