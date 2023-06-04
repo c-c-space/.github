ef55fdf0d8fb5e6fe3a7c9c90ec2250c1c0f26c5
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-04 22:54:25 +0900

    Update log.php

--- hello/all/log.php
+++ hello/all/log.php
@@ -1,17 +1,5 @@
 <section>
   <ul>
-    <li>
-      <p>
-        <code>en-GB</code>
-        <code>Pitch 0.9</code>
-        <code>Rate 0.9</code>
-      </p>
-      <p>
-        <button type="button" data-name="" lang="ja" data-pitch="0.9" data-rate="0.9" data-hello="<?php echo $hello;?>">
-          <?php echo $date;?>
-        </button>
-      </p>
-    </li>
     <?php if (!empty($rows)) : ?>
       <?php shuffle($rows); foreach ($rows as $row):?>
         <li>
