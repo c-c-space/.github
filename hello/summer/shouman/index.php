a0a65660fa1b4c9d708a4d97954785752e5d7d4d
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-04 12:54:36 +0900

    Update index.php

--- hello/summer/shouman/index.php
+++ hello/summer/shouman/index.php
@@ -1,16 +1,7 @@
 <?php
-require_once('../../all/head.php');
+require_once('../php/head.php');
 ?>
 
 <body>
-  <main id="log">
-    <div>
-      <h1>
-        <b></b><br/>
-        <code id="lastModified"></code>
-      </h1>
-      <h2></h2>
-    </div>
-  </main>
 </body>
 </html>
