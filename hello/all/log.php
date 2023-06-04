19828d1dbe05a94e7828853f568f064cbf726d32
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-04 14:33:37 +0900

    Update log.php

--- hello/all/log.php
+++ hello/all/log.php
@@ -17,6 +17,16 @@
       <?php endforeach; ?>
     <?php else : ?>
       <li>
+        <p>
+          <code>Google 日本語 (ja-JP)</code>
+          <code>Pitch 0.9</code>
+          <code>Rate 0.9</code>
+        </p>
+        <p>
+          <button type="button" data-name="Google 日本語" lang="ja-JP" data-pitch="0.9" data-rate="0.9" data-hello="<?php echo $description;?>">
+            <?php echo $date;?>
+          </button>
+        </p>
       </li>
     <?php endif; ?>
   </ul>
