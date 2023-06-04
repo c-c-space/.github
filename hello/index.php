e0163303fcdced706c28f7470c5e39567bba4ea2
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-04 14:16:29 +0900

    Update index.php

--- hello/index.php
+++ hello/index.php
@@ -117,6 +117,12 @@ flock($fp, LOCK_SH);
         <b><?php echo $season;?></b>
       </h2>
     </div>
+    <?php
+    function h($str) {
+      return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+    }
+    require('all/log.php');
+    ?>
   </main>
   <nav id="now" class="hidden">
     <section class="controls">
