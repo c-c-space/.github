172634d3b2ee45f2f3a374f78bdaa053cd3fa210
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-04 12:49:16 +0900

    Update index.php

--- hello/summer/shouman/index.php
+++ hello/summer/shouman/index.php
@@ -13,14 +13,5 @@ require_once('../../all/head.php');
     </div>
     <?php require('../../all/log.php'); ?>
   </main>
-
-  <dialog id="modal" class="color bgcolor">
-    <h3></h3>
-    <button class="color bgcolor" id="closeButton">×</button>
-    <br/>
-    <?php require('../../all/timeframe.php'); ?>
-  </dialog>
-  <script src="../../../profile/js/jscolor.js"></script>
-  <script src="../../../profile/setStyles.js"></script>
 </body>
 </html>
