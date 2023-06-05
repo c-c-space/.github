b3b78445b32e1e39f1d3ba9394787dcb51951ac7
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-05 18:18:03 +0900

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -4,8 +4,8 @@ require('hello/all/24sekki.php');
 
 <b>Welcome ようこそ</b><br>
 Now is The Season named
-<a href="<?php echo $season;?>/<?php echo $sekki;?>/">
-<?php echo $sekkiName;?> (<b><?php echo $sekki;?></b>) 
+<a href="hello/">
+<?php echo $sekkiName;?> (<?php echo $sekki;?>)
 </a>
 in
 <b><?php echo $season;?></b>
