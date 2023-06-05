6ddc775edcbc0d41bf6726cdb8b5f84a9fcf1fa1
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-05 16:25:31 +0900

    all

--- hello/spring/index.php
+++ hello/spring/index.php
@@ -57,28 +57,7 @@ $url = "{$site}" . "{$_SERVER['REQUEST_URI']}";
   <script src="/js/menu.js"></script>
   <header id="menu" class="bgcolor" hidden>
     <button class="color bgcolor" id="js-button"><b></b></button>
-    <nav id="contents">
-      <a href="/hello/">
-        <i>Speech to Text to Text to Speech</i>
-        <p><b><?php echo $greeting; ?></b></p>
-      </a>
-      <a href="/hello/spring">
-        <i>February 4 - May 4</i>
-        <p><b>春</b> Spring</p>
-      </a>
-      <a href="/hello/summer">
-        <i>May 5 - August 7</i>
-        <p><b>夏</b> Summer</p>
-      </a>
-      <a href="/hello/autumn">
-        <i>August 8 - November 7</i>
-        <p><b>秋</b> Autumn</p>
-      </a>
-      <a href="/hello/winter">
-        <i>November 8 - February 3</i>
-        <p><b>冬</b> Winter</p>
-      </a>
-    </nav>
+    <?php require('../all/menu.html'); ?>
   </header>
 
   <main id="log">
