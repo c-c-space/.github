9d41fd87abcd5d6bbc132b70c166f3e52778a2cf
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-05 18:45:29 +0900

    Update index.php

--- hello/index.php
+++ hello/index.php
@@ -127,7 +127,7 @@ fclose($fp);
     </h3>
     <button type="button" class="color bgcolor" id="closeButton">×</button>
     <section id="about">
-      <p><u><?php echo $date;?></u></p>
+      <p><small><?php echo $date;?></small></p>
       <h2>
         <strong><?php echo $sekkiName;?></strong> (<?php echo $sekki;?>)
         is <?php echo $description;?>
