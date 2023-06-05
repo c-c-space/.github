a23b73f4f31497c57b76cca889f65de753bb55a4
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-05 18:49:20 +0900

    Update index.php

--- hello/index.php
+++ hello/index.php
@@ -152,6 +152,7 @@ fclose($fp);
       <option value="16px" selected>Medium</option>
       <option value="20px">Large</option>
     </select>
+    <hr/>
     コントロールから日本の伝統的な季節の色を選択すると、文字・背景の色が変更されます。
   </dialog>
 
