ce2d4576202472e31c5623e1cd74dcf606b8b9ef
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-06 23:43:33 +0900

    Update index.php

--- hello/all/index.php
+++ hello/all/index.php
@@ -340,15 +340,12 @@ $url = "{$site}" . "{$_SERVER['REQUEST_URI']}";
         </li>
       </ul>
     </section>
-  </main>
-
-  <nav id="now" class="hidden">
     <section class="controls">
       <input type="button" class="color bgcolor" id="cancel-btn" value="⏹">
       <input type="button" class="color bgcolor" id="pause-btn" value="⏸">
       <input type="button" class="color bgcolor" id="resume-btn" value="⏯">
     </section>
-  </nav>
+  </main>
 
   <script type="text/javascript">
   function ChangeHidden() {
