4f599a5318b0b4ed2b4e80e9e5d7fc96d8371a3d
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-07 13:49:28 +0900

    Update menu.php

--- hello/all/menu.php
+++ hello/all/menu.php
@@ -8,7 +8,7 @@
     <i><?php echo $seasonDate; ?></i>
     <p><b><?php echo $seasonName; ?></b> <?php echo $season; ?></p>
   </a>
-  <a onclick="window.location.replace('/hello/<?php echo $season; ?>/<?php echo $sekki; ?>/');">
+  <a onclick="window.location.assign('/hello/<?php echo $season; ?>/<?php echo $sekki; ?>/');">
     <i>Now is the season named</i>
     <p><b><?php echo $sekkiName; ?></b> <?php echo $sekki; ?></p>
   </a>
