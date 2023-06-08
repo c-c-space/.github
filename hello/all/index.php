e8dcca475c8f0e5bdfffe83e169268b173fd5196
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-08 14:33:54 +0900

    Update index.php

--- hello/all/index.php
+++ hello/all/index.php
@@ -110,8 +110,8 @@ $url = "{$site}" . "{$_SERVER['REQUEST_URI']}";
     <hr/>
     <br>
     <section id="readme">
-      The Year is divided into Four Seasons, but traditionally the year was also divided up into 24 Sekki.<br/>
-      24 Sekki divided each of the Four Seasons into 6 according to the ecliptic longitude of the Sun.<br/>
+      The Year is divided into Four Seasons, but traditionally the year was also divided up into 24 Sekki in Japan.<br/>
+      24 Sekki divided each of the Four Seasons into 6 according to the ecliptic longitude of the Sun. It was made in China and come to Japan and improved.<br/>
       <br/>
       二十四節気は、四季「春」「夏」「秋」「冬」それぞれを太陽の動きをもとに6つに分け、季節をあらわす名前をつけたもの。<br/>
       中国の中原の気候をもとに作られたため、日本で体感する気候とは季節感が合わない名称や時期があります。また、その年によって節の始まりの日が1日程度前後することがあります。<br/>
