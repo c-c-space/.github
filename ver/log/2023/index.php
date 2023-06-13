c10e77347f28ec10d84557bdcf6eac4aee12677f
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-13 16:46:19 +0900

    Update index.php

--- ver/log/2023/index.php
+++ ver/log/2023/index.php
@@ -168,21 +168,20 @@ flock($fp, LOCK_SH);
 
   <form id="now" method="GET" class="hidden">
     <select id="month" name="month">
-      <select id="month" name="month">
-        <option disabled selected hidden>Select Month</option>
-        <option value="01"><?php echo $year; ?>年1月</option>
-        <option value="02"><?php echo $year; ?>年2月</option>
-        <option value="03"><?php echo $year; ?>年3月</option>
-        <option value="04"><?php echo $year; ?>年4月</option>
-        <option value="05"><?php echo $year; ?>年5月</option>
-        <option value="06"><?php echo $year; ?>年6月</option>
-        <option value="07"><?php echo $year; ?>年7月</option>
-        <option value="08"><?php echo $year; ?>年8月</option>
-        <option value="09"><?php echo $year; ?>年9月</option>
-        <option value="10"><?php echo $year; ?>年10月</option>
-        <option value="11"><?php echo $year; ?>年11月</option>
-        <option value="12"><?php echo $year; ?>年12月</option>
-      </select>
+      <option disabled selected hidden>Select Month</option>
+      <option value="01"><?php echo $year; ?>年1月</option>
+      <option value="02"><?php echo $year; ?>年2月</option>
+      <option value="03"><?php echo $year; ?>年3月</option>
+      <option value="04"><?php echo $year; ?>年4月</option>
+      <option value="05"><?php echo $year; ?>年5月</option>
+      <option value="06"><?php echo $year; ?>年6月</option>
+      <option value="07"><?php echo $year; ?>年7月</option>
+      <option value="08"><?php echo $year; ?>年8月</option>
+      <option value="09"><?php echo $year; ?>年9月</option>
+      <option value="10"><?php echo $year; ?>年10月</option>
+      <option value="11"><?php echo $year; ?>年11月</option>
+      <option value="12"><?php echo $year; ?>年12月</option>
+    </select>
     <button type="submit">Access Log</button>
   </form>
   <script src="../../../thankyou/hsl.js"></script></body>
