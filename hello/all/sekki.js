1fc5e9a89e84a2b77ed627b4f5e6623d8b4a8ce6
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-16 17:39:46 +0900

    spring

---
+++ hello/all/sekki.js
@@ -0,0 +1,97 @@
+'use strict'
+
+async function sekkiJSON(requestURL) {
+  const request = new Request(requestURL);
+  const response = await fetch(request);
+  const jsonSekki = await response.text();
+
+  const sekkiAll = JSON.parse(jsonSekki);
+  sekkiIndex(sekkiAll);
+
+  const collection = document.querySelector('#collection');
+  if(!localStorage.getItem('yourInfo')) {
+    collection.remove()
+  } else {
+    sekkiSekect(sekkiAll)
+  }
+}
+
+function sekkiIndex(obj) {
+  let ul = document.querySelector('#log ul')
+  const allSekki = obj.sekki;
+  for (const sekki of allSekki) {
+    let li = document.createElement('li')
+    let date = document.createElement('p')
+    date.innerHTML = `
+    <date>${sekki.start}</date> -
+    <date>${sekki.end}</date>
+    `
+    let button = document.createElement('button')
+    button.type = 'button';
+    button.setAttribute('data-name', sekki.name)
+    button.setAttribute('data-note', sekki.note)
+    button.setAttribute('data-description', sekki.description)
+    button.innerText = sekki.yomi
+    ul.appendChild(li)
+    li.appendChild(date)
+    li.appendChild(button)
+
+    button.addEventListener('click', function () {
+      const uttr = new SpeechSynthesisUtterance()
+      uttr.text = this.innerText + this.dataset.description
+      uttr.lang = "ja-JP"
+      uttr.pitch = 0.9
+      uttr.rate = 0.9
+      speechSynthesis.speak(uttr)
+
+      const sekkiName = document.querySelector('#log h1 b')
+      sekkiName.innerText = this.dataset.name + this.innerText
+
+      const sekkiDates = document.querySelector('#lastModified')
+      sekkiDates.innerText = this.dataset.description
+
+      const sekkiAbout = document.querySelector('#log h2')
+      sekkiAbout.innerText = this.dataset.note
+    }, false)
+  }
+}
+
+function sekkiSekect(obj) {
+  let select = document.querySelector('#sekki')
+  const allSekki = obj.sekki;
+  for (const sekki of allSekki) {
+    let option = document.createElement('option')
+    option.value = sekki.value
+    option.innerText = sekki.start + ' - ' + sekki.end
+    select.appendChild(option)
+  }
+}
+
+// 発話の停止・一時停止・再開
+const cancelBtn = document.querySelector('#cancel-btn')
+const pauseBtn = document.querySelector('#pause-btn')
+const resumeBtn = document.querySelector('#resume-btn')
+
+cancelBtn.addEventListener('click', function () {
+  speechSynthesis.cancel()
+})
+
+pauseBtn.addEventListener('click', function () {
+  speechSynthesis.pause()
+})
+
+resumeBtn.addEventListener('click', function () {
+  speechSynthesis.resume()
+})
+
+
+document.addEventListener('readystatechange', event => {
+  if (event.target.readyState === 'complete') {
+    const selectModal = document.querySelector('#modal select');
+    const optionModal = document.querySelectorAll("#modal select option");
+    selectModal.addEventListener('change', function() {
+      const index =  this.selectedIndex;
+      window.location.assign(optionModal[index].value);
+    });
+  }
+})
--- hello/spring/index.php
+++ hello/spring/index.php
@@ -73,62 +73,7 @@ $url = "{$site}" . "{$_SERVER['REQUEST_URI']}";
       </h2>
     </div>
     <section>
-      <ul>
-        <li>
-          <p>
-            <date>February 4</date> -
-            <date>February 18</date>
-          </p>
-          <button type="button" data-name="立春" data-date="The Beginning of Spring" data-hello="旧暦では、一年のはじまりは立春から。まだ寒さの厳しい時期だが日脚は徐々に伸び、暖かい地方では梅が咲き始める。">
-            (りっしゅん)
-          </button>
-        </li>
-        <li>
-          <p>
-            <date>February 19</date> -
-            <date>March 4</date>
-          </p>
-          <button type="button" data-name="雨水" data-date="Snow melts into water" data-hello="空から降るものが雪から雨に替わる頃。深く積もった雪も融け始める。草木がほんのり色づく様子や、春霞を楽しめる季節。">
-            (うすい)
-          </button>
-        </li>
-        <li>
-          <p>
-            <date>March 5</date> -
-            <date>March 19</date>
-          </p>
-          <button type="button" data-name="啓蟄" data-date="Insects emerge from the ground" data-hello="冬眠をしていた虫が穴から出てくる頃。蝶々が飛びはじめ、柳の若芽が芽吹き、蕗のとうの花や桃の花が咲く頃。">
-            (けいちつ)
-          </button>
-        </li>
-        <li>
-          <p>
-            <date>March 20</date> -
-            <date>April 3</date>
-          </p>
-          <button type="button" data-name="春分" data-date="The Spring Equinox" data-hello="春分日をはさんで前後7日間が彼岸。雀が巣をつくり始め、桜が開花する頃。花冷えや寒の戻りがあるので暖かいと言っても油断は禁物。この後は昼の時間が長くなって行く。">
-            (しゅんぶん)
-          </button>
-        </li>
-        <li>
-          <p>
-            <date>April 4</date> -
-            <date>April 18</date>
-          </p>
-          <button type="button" data-name="清明" data-date="Clear and Bright, Plants flower" data-hello="清浄明潔の略。晴れ渡った空には当に清浄明潔という語にふさわしい。つばめが飛来し、爽やかな風が吹く頃。地上に目を移せば、百花が咲き競う。">
-            (せいめい)
-          </button>
-        </li>
-        <li>
-          <p>
-            <date>April 19</date> -
-            <date>May 4</date>
-          </p>
-          <button type="button" data-name="穀雨" data-date="Spring rains and seed sowing" data-hello="田んぼや畑の準備が整い、それに合わせるように柔らかな春の雨が降る頃。この頃より変りやすい春の天気も安定し日差しも強まる。植物が緑一色に輝きはじめる。">
-            (こくう)
-          </button>
-        </li>
-      </ul>
+      <ul></ul>
     </section>
   </main>
 
@@ -140,12 +85,6 @@ $url = "{$site}" . "{$_SERVER['REQUEST_URI']}";
     <section id="collection">
       <select class="color bgcolor" id="sekki">
         <option selected disabled>View The Collection</option>
-        <option value="risshun">February 4 - February 18</option>
-        <option value="usui">February 19 - March 4</option>
-        <option value="keichitsu">March 5 - March 19</option>
-        <option value="shunbun">March 20 - April 3</option>
-        <option value="seimei">April 4 - April 18</option>
-        <option value="kokuu">April 19 - May 4</option>
       </select>
       <p><label for="sekki">コントロールを選択すると、二十四節気ごとの投稿一覧ページに移動します。</label></p>
       <hr>
@@ -177,13 +116,21 @@ $url = "{$site}" . "{$_SERVER['REQUEST_URI']}";
       コントロールから日本の伝統的な季節の色を選択すると、文字・背景の色が変更されます。
     </form>
   </dialog>
-  <?php require('../all/controls.html'); ?>
+  <nav id="now" class="hidden">
+    <section class="controls">
+      <input type="button" class="color bgcolor" id="cancel-btn" value="⏹">
+      <input type="button" class="color bgcolor" id="pause-btn" value="⏸">
+      <input type="button" class="color bgcolor" id="resume-btn" value="⏯">
+    </section>
+    <button id="openModal" class="color bgcolor" type="button">?</button>
+  </nav>
 
   <script src="../all/controls.js"></script>
+  <script src="../all/sekki.js"></script>
   <script type="text/javascript">
-    colorJSON('color.json');
+    colorJSON('color.json')
+    sekkiJSON('sekki.json')
   </script>
-  <script src="../all/24sekki.js"></script>
   <script src="../../profile/js/setStyles.js"></script>
 </body>
 </html>
