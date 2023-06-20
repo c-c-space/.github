199a44779d683ec62375fd7833e0199f24c3679b
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-21 02:19:15 +0900

    cc

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no" />
   <?php require('ver/icon.html');?>
-
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
   <script src="js/index.js" async></script>
   <script src="js/hello.js"></script>
@@ -19,7 +18,20 @@
   <link rel="stylesheet" href="thankyou/style.css" />
   <link rel="stylesheet" href="thankyou/www.css" media="print"/>
   <link rel="stylesheet" href="hello/css/mobile.css" media="screen and (max-width: 750px)" />
-  <link rel="stylesheet" href="style.css" />
+  <style>
+  #js-button,
+  #contents a,
+  main {
+    filter: invert();
+  }
+
+  header,
+  main,
+  #sketch,
+  #userMedia {
+    mix-blend-mode: difference;
+  }
+  </style>
 </head>
 <body>
   <header id="menu" hidden>
