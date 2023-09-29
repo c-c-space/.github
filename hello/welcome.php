23bb1b4703b116db612437367ef54bf08b6ae5b3
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-29 22:23:38 +0900

    CC

--- hello/welcome.php
+++ hello/welcome.php
@@ -1,10 +1,12 @@
 <?php
+//現在の日時を取得
+$datetime = date('m-d');
 require('all/24sekki.php');
 ?>
 
 <b>Welcome ようこそ</b><br>
 Now is The Season named
 <a href="/hello/">
-<?php echo $sekkiName;?> (<?php echo $sekki;?>)
+    <?php echo $sekkiName; ?> (<?php echo $sekki; ?>)
 </a>
-in <b><?php echo $season;?></b>
+in <b><?php echo $season; ?></b>
