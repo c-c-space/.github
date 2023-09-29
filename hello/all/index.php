041c9d16a0d622c25c727b47f293b55b5758e3a3
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-29 21:38:46 +0900

    Hello

--- hello/all/index.php
+++ hello/all/index.php
@@ -86,14 +86,10 @@
 
   <dialog id="modal">
     <button type="button" id="closeModal">×</button>
-    <section id="color-size"></section>
   </dialog>
 
-  <script src="../js/colorJSON.js"></script>
   <script src="24sekki.js"></script>
   <script type="text/javascript">
-    colorSize()
-
     sekkiJSON('../spring/sekki.json');
     sekkiJSON('../summer/sekki.json');
     sekkiJSON('../autumn/sekki.json');
