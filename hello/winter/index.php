263cbf9194a477c542d85ea07af53932a51e288a
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-29 12:44:12 +0900

    Hello

--- hello/winter/index.php
+++ hello/winter/index.php
@@ -31,6 +31,7 @@
       <select id="sekki">
         <option selected disabled>View The Collection</option>
       </select>
+      <br>
       <label for="sekki">コントロールを選択すると、二十四節気ごとの投稿一覧ページに移動します。</label>
     </section>
     <div>
