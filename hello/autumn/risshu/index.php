9482c69c5a2915e94ad67aa52522d19de320dbe7
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-01-29 21:27:31 +0900

    Update index.php

--- hello/autumn/risshu/index.php
+++ hello/autumn/risshu/index.php
@@ -5,7 +5,7 @@
   <?php
   $datetime = date('08-08');
   require_once('../../all/24sekki.php');
-  require_once('../../all/greeting.php');
+  require_once('../../greeting.php');
 
   $title = $sekkiName . ' | ' . $date;
   $thisDescription = $description . ' | ' . $hello;
