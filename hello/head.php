4f06b00c4aa0195e60859018cbfff4bb48c68c9b
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-01-29 21:08:13 +0900

    Update head.php

--- hello/head.php
+++ hello/head.php
@@ -9,16 +9,29 @@ $url = "{$site}" . "{$_SERVER['REQUEST_URI']}";
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name="format-detection" content="telephone=no" />
+<link rel="icon" href="/ver/icon/favicon.png" type="image/png">
+
+<!-- HTML Meta Tags -->
+<title><?php echo $title; ?></title>
+<meta name="description" content="<?php echo $thisDescription; ?>">
+
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="<?php echo $url; ?>">
+<meta property="og:type" content="website">
+<meta property="og:title" content="<?php echo $title; ?>">
+<meta property="og:description" content="<?php echo $thisDescription; ?>">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="creative-community.space">
+<meta property="twitter:url" content=<?php echo $url; ?>">
+<meta name="twitter:title" content="<?php echo $title; ?>">
+<meta name="twitter:description" content="<?php echo $thisDescription; ?>">
+
 <script src="/ver/js/menu.js"></script>
 <script src="/ver/js/modal.js"></script>
 
 <!--og:meta-->
-<meta content="website" property="og:type">
-<title><?php echo $title; ?></title>
-<meta content="<?php echo $title; ?>" property="og:title">
-<meta content="<?php echo $thisDescription; ?>" name="description">
-<meta content="<?php echo $thisDescription; ?>" name="og:description">
-<link rel="icon" href="/ver/icon/favicon.png" type="image/png">
 
 <link rel="stylesheet" href="/ver/css/menu.css" />
 <link rel="stylesheet" href="/ver/css/controls.css" />
