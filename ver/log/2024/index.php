2c4f1ca8f3062d0a4925bc7f9c151a489d51f92d
∧° ┐ <we.are.pe.hu@gmail.com>
2024-01-30 11:03:47 +0900

    Update index.php

--- ver/log/2024/index.php
+++ ver/log/2024/index.php
@@ -111,23 +111,10 @@
   </ul>
 
   <form id="now" method="GET" class="hidden">
-    <select id="month" name="month">
-      <option disabled selected hidden>Select Month</option>
-      <option value="01"><?php echo $year; ?>年1月</option>
-      <option value="02"><?php echo $year; ?>年2月</option>
-      <option value="03"><?php echo $year; ?>年3月</option>
-      <option value="04"><?php echo $year; ?>年4月</option>
-      <option value="05"><?php echo $year; ?>年5月</option>
-      <option value="06"><?php echo $year; ?>年6月</option>
-      <option value="07"><?php echo $year; ?>年7月</option>
-      <option value="08"><?php echo $year; ?>年8月</option>
-      <option value="09"><?php echo $year; ?>年9月</option>
-      <option value="10"><?php echo $year; ?>年10月</option>
-      <option value="11"><?php echo $year; ?>年11月</option>
-      <option value="12"><?php echo $year; ?>年12月</option>
-    </select>
+    <select id="month" name="month"></select>
     <button type="submit">Access Log</button>
   </form>
+  <script src="../../js/selectmonth.js"></script>
 </body>
 
 </html>
