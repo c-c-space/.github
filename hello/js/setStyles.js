f804f02d7cd6605142f76943d1861af6b144abcf
∧° ┐ <we.are.pe.hu@gmail.com>
2024-01-31 18:04:15 +0900

    Update setStyles.js

--- hello/js/setStyles.js
+++ hello/js/setStyles.js
@@ -34,36 +34,40 @@ document.addEventListener('readystatechange', event => {
       storage.setItem('color', colorForm.value)
       setStyles()
     }
-
-    function setStyles() {
-      const currentSize = storage.getItem('fontSize')
-      const currentBG = storage.getItem('bgcolor')
-      const currentColor = storage.getItem('color')
-
-      fontSize.value = currentSize;
-      bgcolorForm.value = currentBG;
-      colorForm.value = currentColor;
-
-      const mainAll = document.querySelectorAll('html')
-      for (const main of mainAll) {
-        main.style.fontSize = currentSize;
-      }
-
-      const bgcolorAll = document.querySelectorAll('body, .bgcolor, #modal')
-      for (const bgcolor of bgcolorAll) {
-        bgcolor.style.backgroundColor = currentBG;
-      }
-
-      const colorAll = document.querySelectorAll('body, .color, #modal')
-      for (const color of colorAll) {
-        color.style.color = currentColor;
-      }
-
-      console.info(
-        "Font-Size:", currentSize,
-        "Background-Color:", currentBG,
-        "Color:", currentColor
-      )
-    }
   }
 })
+
+function setStyles() {
+  const currentSize = storage.getItem('fontSize')
+  const currentBG = storage.getItem('bgcolor')
+  const currentColor = storage.getItem('color')
+
+  const fontSize = document.querySelector('#fontSize')
+  const bgcolorForm = document.querySelector('#bgcolor')
+  const colorForm = document.querySelector('#color')
+
+  fontSize.value = currentSize;
+  bgcolorForm.value = currentBG;
+  colorForm.value = currentColor;
+
+  const mainAll = document.querySelectorAll('html')
+  for (const main of mainAll) {
+    main.style.fontSize = currentSize;
+  }
+
+  const bgcolorAll = document.querySelectorAll('body, .bgcolor, #modal')
+  for (const bgcolor of bgcolorAll) {
+    bgcolor.style.backgroundColor = currentBG;
+  }
+
+  const colorAll = document.querySelectorAll('body, .color, #modal')
+  for (const color of colorAll) {
+    color.style.color = currentColor;
+  }
+
+  console.info(
+    "Font-Size:", currentSize,
+    "Background-Color:", currentBG,
+    "Color:", currentColor
+  )
+}
